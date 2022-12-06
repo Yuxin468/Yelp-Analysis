09a981a0230ba3c8d15f524bb9ecdd28c21910d2
Yuxin468 <115087948+Yuxin468@users.noreply.github.com>
2022-12-06 16:07:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import nltk
+nltk.download('stopwords')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from string import punctuation
 from nltk.corpus import stopwords
