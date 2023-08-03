a4281f4013f5a6733d2e775d3a74cac7822f9bb8
Yuxin468 <115087948+Yuxin468@users.noreply.github.com>
2023-08-03 15:50:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -131,10 +131,10 @@ y_group = group[group['name']==business_select][['date_new','sentiment_score']]
 y= np.array(y_group['sentiment_score'].groupby(group['date_new']).mean())
 hue = np.ones(len(y)) *avg_all_senti
 fig_2 = plt.figure(figsize=(5, 3))
-ax = sns.lineplot(y = y, x= x)
-ax.xaxis.set_major_locator(mdates.WeekdayLocator(interval=3))
-plt.plot(x,hue)
-plt.legend(title='Sentiment Scores', loc='lower left', labels=['Scores of this business', 'Average Scores of all business'],fontsize = 'x-small')
+#ax = sns.lineplot(y = y, x= x)
+#ax.xaxis.set_major_locator(mdates.WeekdayLocator(interval=3))
+#plt.plot(x,hue)
+#plt.legend(title='Sentiment Scores', loc='lower left', labels=['Scores of this business', 'Average Scores of all business'],fontsize = 'x-small')
 
 # generate tfidf
 from nltk.corpus import stopwords
